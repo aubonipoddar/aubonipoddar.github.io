efd9b2baa0f3bf636b061a5733b2ef555faa1c7b
aubonipoddar <55279274+aubonipoddar@users.noreply.github.com>
2021-04-20 15:57:53 -0700

    Add files via upload
    trial 4.20.2021

---
+++ cs.js
@@ -0,0 +1,27 @@
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+    var element = document.getElementById("title");
+  element.classList.toggle("mystyle");
+  var elementtwo = document.getElementById("white");
+  elementtwo.classList.toggle("mystyletwo");
+  var elementthree = document.getElementById("whites");
+  elementthree.classList.toggle("mystylethree");
+  var elementfour = document.getElementById("whitess");
+  elementfour.classList.toggle("mystylefour");
+  var elementfive = document.getElementById("whitesss");
+  elementfive.classList.toggle("mystylefive");
+  var elementsix = document.getElementById("whitessss");
+  elementsix.classList.toggle("mystylesix");
+  var elementseven = document.getElementById("web");
+  elementseven.classList.toggle("mystyleseven");
+  var elementeight = document.getElementById("c");
+  elementeight.classList.toggle("mystyleeight");
+  var elementnine = document.getElementById("cc");
+  elementnine.classList.toggle("mystylenine");
+  var elementten = document.getElementById("whitesssss");
+  elementten.classList.toggle("mystyletten");
+  
+ 
+});
